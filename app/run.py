1186aabd3f707512f66034ac97ea1a8897c84782
Bruno12leonel <57578259+Bruno12leonel@users.noreply.github.com>
2023-11-27 16:50:22 -0300

    Update run.py

--- app/run.py
+++ app/run.py
@@ -21,15 +21,18 @@ if __name__ == "__main__":
 
     #denstream = CoreStream(m_minPoints= int(sys.argv[2]) , n_samples_init= int(sys.argv[3]), epsilon= float(sys.argv[4]))   
 
-    corestream = CoreStream(int(sys.argv[3]),
+    corestream = CoreStream(int(sys.arg[3]),
                         min_cluster_size = 25,
                         step=2,
                         decaying_factor=0.025,
-                        mu=3, n_samples_init=7000, 
+                        mu=2, n_samples_init=initial_points, 
                         epsilon=0.005,
                         percent=0.15,
                         method_summarization='single',
-                        stream_speed=100)#  0 < percent <= 1.0
+                        stream_speed=100,
+                        runtime=False,
+                        plot=False,
+                        save_partitions=True)
 
     count_points = 0
 
@@ -40,5 +43,5 @@ if __name__ == "__main__":
         
         if not (count_points % initial_points) and count_points != initial_points:
             corestream.predict_one()
-            corestream.save_runtime_timestamp()
-    corestream.save_runtime_final()
+            #corestream.save_runtime_timestamp()
+    #corestream.save_runtime_final()
