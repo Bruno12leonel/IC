7feb57c29d4fdc55ca004524cab0e597e86ca460
Bruno12leonel <57578259+Bruno12leonel@users.noreply.github.com>
2024-03-09 14:07:15 -0300

    Update run.py

--- app_hastream/run.py
+++ app_hastream/run.py
@@ -2650,9 +2650,6 @@ if __name__ == "__main__":
 
     data = pd.read_csv(sys.argv[1], sep=',')
     initial_points = int(sys.argv[2])
-    runtime = bool(sys.argv[5])
-    plot = bool(sys.argv[6])
-    save_partitions = bool(sys.argv[7])
     
     scaler = MinMaxScaler()
 
@@ -2673,9 +2670,9 @@ if __name__ == "__main__":
                       stream_speed=100,
                       percent=0.15,
                       method_summarization='single',
-                      runtime=runtime,
-                      plot=plot,
-                      save_partitions=save_partitions)
+                      runtime=True,
+                      plot=False,
+                      save_partitions=True)
 
     count_points = 0
     
