357ff74bbc41d6a784db184b247eab774a0ba675
Bruno12leonel <bruno12leonel@gmail.com>
2025-05-14 23:15:12 -0300

    Refactor setup.py to use setuptools and simplify extension configuration

--- core_ssg/setup.py
+++ core_ssg/setup.py
@@ -1,28 +1,17 @@
-from distutils.core import setup
-from distutils.extension import Extension
+from setuptools import setup, Extension
 from Cython.Build import cythonize
-
 import numpy
 
 extensions = [
     Extension(
         "mst.mst",
         ["mst/mst.pyx"],
-        extra_compile_args = ["-ffast-math"]
-    ),
-    Extension(
-        "rng.rng",
-        ["rng/rng.pyx"],
-        extra_compile_args = ["-ffast-math"]
-    ),
-    Extension(
-        "rng.fst.fstree",
-        ["rng/fst/fstree.pyx"],
-        extra_compile_args = ["-ffast-math"]
+        extra_compile_args=["-ffast-math"],
+        include_dirs=[numpy.get_include()]
     )
 ]
 
 setup(
     name='hdbscan_python',
-    ext_modules=cythonize(extensions, include_path = [numpy.get_include()], annotate=True)
+    ext_modules=cythonize(extensions, annotate=True)
 )
